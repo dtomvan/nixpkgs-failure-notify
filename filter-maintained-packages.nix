1f1fed71c744f7881a24f027048b5b2bb846c554
Tom van Dijk <18gatenmaker6@gmail.com>
2026-04-15 19:56:09 +0200

    chore: format

--- filter-maintained-packages.nix
+++ filter-maintained-packages.nix
@@ -10,34 +10,42 @@ let
     builtins.readFile
     (pkgs.lib.strings.splitString "\n")
     builtins.tail
-    (builtins.map (pkgs.lib.strings.splitString ","))
+    (map (pkgs.lib.strings.splitString ","))
   ];
 
   gh = "dtomvan";
-in let
-  lookupAttrPath = pathStr:
-    builtins.foldl'
-    (acc: p:
-      if acc == null then null
-      else if builtins.hasAttr p acc then builtins.getAttr p acc
-      else null)
-    pkgs
-    (pkgs.lib.strings.splitString "." pathStr);
+in
+let
+  lookupAttrPath =
+    pathStr:
+    builtins.foldl' (
+      acc: p:
+      if acc == null then
+        null
+      else if builtins.hasAttr p acc then
+        builtins.getAttr p acc
+      else
+        null
+    ) pkgs (pkgs.lib.strings.splitString "." pathStr);
 
   isPkgMaintainer = pkg: pkgs.lib.any (m: (m.github or "") == gh) pkg;
 
-  getMaintainers = pkg: pkg.meta.maintainers or [];
+  getMaintainers = pkg: pkg.meta.maintainers or [ ];
 
-  isMaintainer = pkg-build-fail: let
-    evald = builtins.tryEval (
-      pkgs.lib.pipe pkg-build-fail [
-        builtins.head
-        lookupAttrPath
-        getMaintainers
-        isPkgMaintainer
-      ]
-    );
+  isMaintainer =
+    pkg-build-fail:
+    let
+      evald = builtins.tryEval (
+        pkgs.lib.pipe pkg-build-fail [
+          builtins.head
+          lookupAttrPath
+          getMaintainers
+          isPkgMaintainer
+        ]
+      );
 
-    in evald.success && evald.value;
+    in
+    evald.success && evald.value;
 
-in builtins.filter isMaintainer failures-packed
+in
+builtins.filter isMaintainer failures-packed
